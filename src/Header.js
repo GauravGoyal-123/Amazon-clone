a265acec58bd93b03f0091ada206269c2c06edfc
Gaurav Goyal <er.gauravgoyal786@gmail.com>
2023-02-07 15:05:19 +0530

    second day of amazon-clone

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import './Header.css'
-
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 const Header = () => {
   return (
     <div className='header'>
         <img src = "https://pngimg.com/uploads/amazon/amazon_PNG11.png"  className='header-logo' alt = 'amagon-logo' />
         <div className='header-search'> 
             <input className='header-input' type="text" />
-            {/*logo */}
+           
+            <SearchIcon className='header-search-icon' />
 
         </div>
 
@@ -25,6 +27,11 @@ const Header = () => {
             <div className='header-option'>
                 <span className='header-option-one' >Your</span>
                 <span className='header-option-two'>Prime</span>
+            </div>
+
+            <div className='header-option-basket'>
+                <ShoppingBasketIcon/>
+                <span className='header-option-two header-basket-count' >0</span>
             </div>
         </div>
 
---
+++ src/Product.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Product.css'
+function Product() {
+  return (
+    <div className='product'>
+        <div className='product-info'>
+            <p>The Product</p>
+            <small>$</small>
+            <strong>19.99</strong>
+        </div>
+    </div>
+  )
+}
+
+export default Product
